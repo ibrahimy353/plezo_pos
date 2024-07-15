ea27ade1c65ab63c5d1b327713b02994d8d11c58
ibrahimy353 <ibrahimy353@gmail.com>
2024-07-15 20:34:39 +0300

    indroduce the get stated today button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,25 @@
     <div class="h-80 w-full">
         <div class="container size-full mx-auto mt-10">
             <h1 class="text-5xl text-center font-bold">Full-suite POS software for businesses</h1>
-            <p class="mt-14 text-center text-xl">An All-in-One Mobile and Web Point of Sale (POS) software creating
+            <p class="mt-14 text-center text-xl">An All-in-One Mobile and Web Point of Sale (POS) software <br>size-full
+                creating
                 endless
                 possibilities for your Business
                 <br>
                 Access your Business anytime, anywhere
                 <br> Streamline your operations
             </p>
+            <div class="container  mx-auto mt-10 flex justify-center">
+
+                <button
+                    class="border-solid  text-slate-100 items-center  px-5 py-2 bg-violet-800  rounded-lg hover:bg-neutral-400"><a
+                        href=" " clss="nav-link">Get Started Today</a></button>
+            </div>
         </div>
+
     </div>
-    <div class="card h-96 bg-slate-500"></div>
+    <div class="card h-96 bg-slate-500">
+        <img src="https://unsplash.com/photos/graphs-of-performance-analytics-on-a-laptop-screen-JKUTrJ4vK00"
+            alt="">
+    </div>
 </x-layout>
